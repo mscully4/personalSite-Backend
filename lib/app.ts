20ba0afdf0078d26f39548b7f41cc155e38f2034
Michael <michael.scully1997@gmail.com>
2022-05-07 18:41:29 -0400

    Add Storage Stack in CDK

--- lib/app.ts
+++ lib/app.ts
@@ -1,10 +1,13 @@
 import { App } from "aws-cdk-lib";
 import { deploymentEnvironments } from "./constants/deploymentEnvironments";
 import { DeploymentEnvironment } from "./types/DeploymentEnvironment";
+import { StorageStack } from "./stacks/storageStack";
 
 const appName = "michaeljscullydotcom"
 const app = new App();
 
 deploymentEnvironments.forEach((env: DeploymentEnvironment) => {
-
+  const storageStack = new StorageStack(app, `${appName}-storageStack-${env.stage}`, {
+    env: env,
+  });
 });
